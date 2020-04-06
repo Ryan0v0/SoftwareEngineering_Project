f0bea68209a4c7e2e72c24adcd6bb73a0187c6de
Ryan0v0 <sxxhgz@163.com>
2020-04-06 12:35:56 +0800

    设计设备编号、增加删除设备提醒

--- README.md
+++ README.md
@@ -60,6 +60,8 @@
 1. 登录：管理员可以通过输入预置的账号密码进行登录。 
 2. 查看设备列表：管理员在登录成功后，应立即展示所有设备信息，设备 信息应包括设备 ID 号，设备名，实验室名，购置时间，购置人。 
 3. 增加设备：增加设备时应输入设备名，实验室名，购置人等信息，设备 增加成功后自动返回系统分配的设备 ID 号，购置时间应为系统自动生成(默认为增加设备的时间)。
+4. *[附加] 删除设备：管理员本人购置的设备具有顶级重要性，故不能被删除*
+5. *[附加] 搜索设备：输入关键词，显示名称中包含关键词的设备列表*
 
 另外保证：
 
@@ -75,18 +77,154 @@
 
 ## 功能细节
 
-1. 活动图
-2. 用例图
-3. 顺序图
-4. 类图
-5. 状态图
+1、活动图
+<img src="/Users/zhaowanru/Library/Application Support/typora-user-images/image-20200405013518204.png" alt="image-20200405013518204" style="zoom:50%;" />
+
+2、用例图
+
+![image-20200405103612687](/Users/zhaowanru/Library/Application Support/typora-user-images/image-20200405103612687.png)
+
+3、顺序图
+
+<img src="/Users/zhaowanru/Downloads/用户使用系统顺序图 (1).png" alt="用户使用系统顺序图 (1)" style="zoom:50%;" />
+
+4、类图
+
+![类图](/Users/zhaowanru/Downloads/类图.png)
+
+* `app.Role`为⽤户设置的用户或者管理员⻆角⾊类，内部有条件⻆角⾊条件约束 
+
+* `app.User` ⽤户类
+  * 记录⽤户名、密码、id等信息，与数据库的属性产⽣交互 
+* `app.Role`为⽤户设置的用户或者管理员⻆角⾊类，内部有条件⻆角⾊条件约束 
+
+* FlaskForm 信息表 
+  * 四种Form根据不同的操作需求，设定不同的Field
+
+
+5、状态图
+![后台管理界面状态图 (1)](/Users/zhaowanru/Downloads/后台管理界面状态图 (1).png)
+
+
+* `app.py`中function与` templates`中HTML⽂件对应，展示在⽹⻚页中
+  * index() 通过 SearchForm 实现对⽤户信息的检索和展示，并通过 index.html 
+  * add_user() 通过 UserForm 实现对新考⽣信息的添加，并调⽤ add_user.html 
+  * remove_user(id) 通过id删除考⽣，但不能删除管理员
+  * edit_user() 通过调⽤ EditForm 实现考⽣信息的修改，并调⽤ edit_user.html ⽹⻚页修改 
+  * login() 通过调⽤ LoginForm 实现登录，并调⽤ login.html ⽹⻚页登录，也是服务器提供的第⼀个页⾯
+  * 其余function对应⼀系列错误处理和必要但和数据库⽆关功能 
+* HTML使⽤Flask-wtf Bootstrap渲染功能，使界⾯更美观
+
+
+
+⼆、数据库基本表的定义
+
+#### 表一：
+
+| roles 身份表 |             |              |                 |            |      |      |
+| ------------ | ----------- | ------------ | --------------- | ---------- | ---- | ---- |
+| 名称         | 类型        | NOT NULL约束 | PRIMARY KEY约束 | UNIQUE约束 | 默认 | 外键 |
+| id           | INTEGER     | √            | √               |            |      |      |
+| name         | VARCHAR(64) |              |                 |            |      |      |
+
+```mysql
+CREATE TABLE roles (
+	id INTEGER NOT NULL, 
+	name VARCHAR(64), 
+	PRIMARY KEY (id), 
+	UNIQUE (name)
+)
+```
+
+
+表中实体：
+
+| **id** | **name** |
+| ------ | -------- |
+| **1**  | User     |
+| **2**  | Admin    |
+
+#### 表二：
+
+| users 用户表  |              |              |                 |            |      |           |
+| ------------- | ------------ | ------------ | --------------- | ---------- | ---- | --------- |
+| 名称          | 类型         | NOT NULL约束 | PRIMARY KEY约束 | UNIQUE约束 | 默认 | 外键      |
+| id            | INTEGER      | √            | √               |            |      |           |
+| number        | VARCHAR(128) |              |                 |            |      |           |
+| username      | VARCHAR(64)  |              |                 |            |      |           |
+| password_hash | VARCHAR(128) |              |                 |            |      |           |
+| role_id       | INTEGER      |              |                 |            |      | roles(id) |
+
+
+```mysql
+CREATE TABLE users (
+	id INTEGER NOT NULL, 
+	number VARCHAR(128), 
+	username VARCHAR(64), 
+	password_hash VARCHAR(128), 
+	role_id INTEGER, 
+	PRIMARY KEY (id), 
+	UNIQUE (password_hash), 
+	FOREIGN KEY(role_id) REFERENCES roles (id)
+)
+```
+
+表中实体：
+
+| id   | number                  | username | password_hash                                                | role_id |
+| ---- | ----------------------- | -------- | ------------------------------------------------------------ | ------- |
+| 1    | zhaowrenee@gmail.com    | Admin    | pbkdf2:sha256:50000$V4ZV7KEr$42b0a9825baa100fa800d0544632a1ad7d6130504ef6c397ecfa6b02ca99298d | 2       |
+| 2    | mshi@hotmail.com        | 袁帅     | pbkdf2:sha256:50000$KWFWUdY9$d1c334fe90415612c0d99301d45f3e5f9c3f482726cde598259d19ff1228c217 | 1       |
+| 3    | yaoxiuying@mingding.org | 刘东     | pbkdf2:sha256:50000$rNv7gYzZ$381113b96a85934496fe06a2796893fb85689b71c373e97a5e4d9d3adfd5ef26 | 1       |
+| 4    | vfan@hotmail.com        | 吴娟     | pbkdf2:sha256:50000$cA7RmNVD$7136167bd1c3163b0ec221638b8644f12f093f3450ecc4f631c72840e26f31f3 | 1       |
+| 5    | panjuan@hotmail.com     | 刘玉珍   | pbkdf2:sha256:50000$hSqUMhqa$5dd222a191f376127e3dceb8244310cc7114dcceaa2d3bc5fa585a9429f66a02 | 1       |
+
+#### 表三：
+
+| devices 设备表 |             |              |                 |            |      |           |
+| -------------- | ----------- | ------------ | --------------- | ---------- | ---- | --------- |
+| 名称           | 类型        | NOT NULL约束 | PRIMARY KEY约束 | UNIQUE约束 | 默认 | 外键      |
+| id             | INTEGER     | √            | √               |            |      |           |
+| lab            | VARCHAR(64) |              |                 |            |      |           |
+| name           | VARCHAR(64) |              |                 |            |      |           |
+| password_hash  | DATETIME    |              |                 |            |      |           |
+| user_id        | VARCHAR(64) |              |                 |            |      | users(id) |
+
+```sqlite
+CREATE TABLE devices (
+	id INTEGER NOT NULL, 
+	lab VARCHAR(64), 
+	name VARCHAR(64), 
+	time DATETIME, 
+	user_id VARCHAR(64), 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(user_id) REFERENCES users (id)
+)
+```
+表中实体：
+
+| id   | device_id   | lab                | name           | time                       | user_id |
+| ---- | ----------- | ------------------ | -------------- | -------------------------- | ------- |
+| 2    | 2020-QS-002 | 趋势传媒实验室     | 专业VR镜头     | 2020-03-31 22:12:17.000000 | 6       |
+| 3    | 2020-LY-003 | 凌云科技实验室     | 专业VR镜头     | 2020-03-15 02:36:25.000000 | 6       |
+| 4    | 2020-MX-004 | 盟新传媒实验室     | 联想启天2100   | 2020-03-16 23:44:42.000000 | 1       |
+| 5    | 2020-CH-005 | 创汇网络实验室     | DSP实验箱      | 2020-01-17 03:29:31.000000 | 8       |
+| 6    | 2020-LT-006 | 联通时科网络实验室 | 功率变换器     | 2020-04-06 07:49:29.000000 | 3       |
+| 7    | 2020-HR-007 | 鸿睿思博网络实验室 | 双踪示波器     | 2020-03-05 00:48:16.000000 | 4       |
+| 8    | 2020-TY-008 | 天益科技实验室     | 联想启天2100   | 2020-03-03 23:57:45.000000 | 11      |
+| 9    | 2020-LY-009 | 凌颖信息信息实验室 | 投影机         | 2020-02-03 20:11:21.000000 | 10      |
+| 10   | 2020-DM-010 | 迪摩科技实验室     | 曙光天阔服务器 | 2020-02-12 20:46:27.000000 | 1       |
+| 11   | 2020-LR-011 | 联软传媒实验室     | 联想启天2100   | 2020-02-21 11:02:09.000000 | 11      |
 
 
 
 ## 展示后修改及优化说明
 
-添加了输入处理
+1. 对设备ID进行改进
+
+2. 删除设备时
 
 
 
-附加了删除设备记录的功能
+## 效果截图
+
--- app.py
+++ app.py
@@ -1,36 +1,40 @@
-#coding=utf-8
+# coding=utf-8
 import os
 from datetime import datetime
 from flask import Flask, render_template, session, redirect, \
-				  url_for, flash, current_app, request
+    url_for, flash, current_app, request
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 from flask_bootstrap import Bootstrap
 from flask_login import UserMixin, LoginManager, login_required, \
-						login_user, logout_user, current_user
+    login_user, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, SelectField, \
-					BooleanField, IntegerField, ValidationError
+    BooleanField, IntegerField, ValidationError
 from wtforms.validators import DataRequired, Required, Length, Regexp
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
 
 import random
+from xpinyin import Pinyin
 from faker import Faker
-fake = Faker('zh_CN')
 
+fake = Faker('zh_CN')
+total=15
 '''
 Config
 '''
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 def make_shell_context():
-	return dict(app=app, db=db, Device=Device, User=User, Role=Role)
+    return dict(app=app, db=db, Device=Device, User=User, Role=Role)
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-	'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['AdminPassword'] = 666666
@@ -47,214 +51,234 @@ login_manager.session_protection = 'strong'
 login_manager.login_view = 'login'
 login_manager.login_message = u"你需要登录才能访问这个页面."
 
-
 '''
 Models
 '''
 
+
 class Role(db.Model):
-	__tablename__ = 'roles'
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(64), unique=True)
-	users = db.relationship('User', backref='role', lazy='dynamic')
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
-	@staticmethod
-	def insert_roles():
-		roles = ('Student', 'Admin')
-		for r in roles:
-			role = Role.query.filter_by(name=r).first()
-			if role is None:
-				role = Role(name=r)
-			db.session.add(role)
-		db.session.commit()
+    @staticmethod
+    def insert_roles():
+        roles = ('Student', 'Admin')
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            db.session.add(role)
+        db.session.commit()
 
-	def __repr__(self):
-		return '<Role %r>' % self.name
+    def __repr__(self):
+        return '<Role %r>' % self.name
 
 
 class User(UserMixin, db.Model):
-	__tablename__ = 'users'
-	id = db.Column(db.Integer, primary_key=True)
-	number = db.Column(db.SmallInteger, unique=True, index=True)
-	username = db.Column(db.String(64), unique=True,index=True)
-	# password = db.Column(db.String(128), default=123456)
-	password_hash = db.Column(db.String(128), unique=True, default=123456)
-	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-	devices = db.relationship('Device', backref='user', lazy='dynamic')
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.SmallInteger, unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    # password = db.Column(db.String(128), default=123456)
+    password_hash = db.Column(db.String(128), unique=True, default=123456)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    devices = db.relationship('Device', backref='user', lazy='dynamic')
 
-	def __init__(self, **kwargs):
-		super(User, self).__init__(**kwargs)
-		# 新添加的用户，初始其角色为学生。
-		if self.role is None:
-			self.role = Role.query.filter_by(name='Student').first()
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        # 新添加的用户，初始其角色为学生。
+        if self.role is None:
+            self.role = Role.query.filter_by(name='Student').first()
 
-	def __repr__(self):
-		return '<User %r>' % self.username
+    def __repr__(self):
+        return '<User %r>' % self.username
 
-	def validate_password(self, password):
-		return check_password_hash(self.password_hash, password)
+    def validate_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
-	def set_password(self, password):
-		self.password_hash = generate_password_hash(password)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-	# 初次运行程序时生成初始管理员的静态方法
-	@staticmethod
-	def generate_admin():
-		admin = Role.query.filter_by(name='Admin').first()
-		u = User.query.filter_by(role=admin).first()
-		if u is None:
-			u = User(number='zhaowrenee@gmail.com', username='Admin', role=Role.query.filter_by(name='Admin').first())
-			u.set_password('666666')
-			db.session.add(u)
-		db.session.commit()
+    # 初次运行程序时生成初始管理员的静态方法
+    @staticmethod
+    def generate_admin():
+        admin = Role.query.filter_by(name='Admin').first()
+        u = User.query.filter_by(role=admin).first()
+        if u is None:
+            u = User(number='zhaowrenee@gmail.com', username='Admin', role=Role.query.filter_by(name='Admin').first())
+            u.set_password('666666')
+            db.session.add(u)
+        db.session.commit()
 
-	def verify_password(self, password):
-		return self.password == password
-
+    def verify_password(self, password):
+        return self.password == password
 
 
 class Device(UserMixin, db.Model):
-	__tablename__ = 'devices'
-	id = db.Column(db.Integer, primary_key=True)
-	lab = db.Column(db.String(64), unique=True, index=True)
-	name = db.Column(db.String(64), index=True)
-	time = db.Column(db.DateTime, default=datetime.utcnow)
-	user_id = db.Column(db.String(64), db.ForeignKey('users.id'))
+    __tablename__ = 'devices'
+    id = db.Column(db.Integer, primary_key=True)
+    device_id = db.Column(db.String(64), unique=True)
+    lab = db.Column(db.String(64), unique=True, index=True)
+    name = db.Column(db.String(64), index=True)
+    time = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.String(64), db.ForeignKey('users.id'))
 
+    def __init__(self, **kwargs):
+        super(Device, self).__init__(**kwargs)
+        # 新添加的实验设备，初始其购置人为管理员。
+        if self.user is None:
+            self.user = User.query.filter_by(username='Admin').first()
 
-	def __init__(self, **kwargs):
-		super(Device, self).__init__(**kwargs)
-		#新添加的实验设备，初始其购置人为管理员。
-		if self.user is None:
-			self.user = User.query.filter_by(username='Admin').first()
+    def __repr__(self):
+        return '<Device %r>' % self.name
 
-	def __repr__(self):
-		return '<Device %r>' %self.name
+    def set_deviceID(self, str):
+        self.device_id = str
 
 
 '''
 Forms
 '''
 
+
 class LoginForm(FlaskForm):
-	number = StringField(u'账号', validators=[DataRequired(), Length(1,32)])
-	password_hash = PasswordField(u'密码', validators=[DataRequired(), Length(1,32)])
-	remember_me = BooleanField(u'记住我')
-	submit = SubmitField(u'登录')
+    number = StringField(u'账号', validators=[DataRequired(), Length(1, 32)])
+    password_hash = PasswordField(u'密码', validators=[DataRequired(), Length(1, 32)])
+    remember_me = BooleanField(u'记住我')
+    submit = SubmitField(u'登录')
 
 
 class SearchForm(FlaskForm):
-	name = StringField(u'设备名', validators=[DataRequired()])
-	submit = SubmitField(u'搜索')
-
+    name = StringField(u'设备名', validators=[DataRequired()])
+    submit = SubmitField(u'搜索')
 
 
 class DeviceForm(FlaskForm):
-	name = StringField(u'设备名', validators=[DataRequired(), Length(1,32)])
-	lab = StringField(u'实验室名', validators=[DataRequired(), Length(1,32)])
-	user_name = StringField(u'购置人')
-	#if not User.query.filter_by(username=user_name.data).first():
-	#	raise ValidationError(u'用户不存在')
-	#validate_name(user_name)
-	# user_id = IntegerField(u'设备号', validators=[Required(message=u'请输入数字')])
-	submit = SubmitField(u'添加')
-	'''
-	def validate_number(self, field):
-		if Device.query.filter_by(id=field.data).first():
-			raise ValidationError(u'此设备已存在，请检查考号！')
-	'''
-	def validate_user_name(self, field):
-		if not User.query.filter_by(username=field.data).first():
-			raise ValidationError(u'用户不存在')
+    name = StringField(u'设备名', validators=[DataRequired(), Length(1, 32)])
+    lab = StringField(u'实验室名', validators=[DataRequired(), Length(1, 32)])
+    user_name = StringField(u'购置人')
+    # if not User.query.filter_by(username=user_name.data).first():
+    #	raise ValidationError(u'用户不存在')
+    # validate_name(user_name)
+    # user_id = IntegerField(u'设备号', validators=[Required(message=u'请输入数字')])
+    submit = SubmitField(u'添加')
+    '''
+    def validate_number(self, field):
+        if Device.query.filter_by(id=field.data).first():
+            raise ValidationError(u'此设备已存在，请检查考号！')
+    '''
+
+    def validate_user_name(self, field):
+        if not User.query.filter_by(username=field.data).first():
+            raise ValidationError(u'用户不存在')
+
+
 '''
 views
 '''
+
+
 @app.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
-	form = SearchForm()
-	admin = Role.query.filter_by(name='Admin').first()
-	if form.validate_on_submit():
-		#获得设备列表，其id包含form中的数字
-		devices = Device.query.filter(Device.name.like('%{}%'.format(form.name.data))).all()
-	else:
-		devices = Device.query.order_by(Device.id.asc(), Device.name.desc()).all()
-	return render_template('index.html', form=form, devices=devices, admin=admin)
+    form = SearchForm()
+    admin = Role.query.filter_by(name='Admin').first()
+    if form.validate_on_submit():
+        # 获得设备列表，其id包含form中的数字
+        devices = Device.query.filter(Device.name.like('%{}%'.format(form.name.data))).all()
+    else:
+        devices = Device.query.order_by(Device.id.asc(), Device.name.desc()).all()
+    return render_template('index.html', form=form, devices=devices, admin=admin)
 
 
-#增加新设备
+# 增加新设备
 @app.route('/add-device', methods=['GET', 'POST'])
 @login_required
 def add_device():
-	form = DeviceForm()
-	if form.validate_on_submit():
-		device = Device(lab=form.lab.data, name=form.name.data, user=User.query.filter_by(username=form.user_name.data).first())
-		db.session.add(device)
-		flash(u'成功添加设备')
-		return redirect(url_for('index'))
-	return render_template('add_device.html', form=form)
+    form = DeviceForm()
+    if form.validate_on_submit():
+        global total
+        total = total + 1
+        print(total)
+        device = Device(lab=form.lab.data, name=form.name.data,
+                        user=User.query.filter_by(username=form.user_name.data).first())
+        device.set_deviceID(
+            "2020-" + Pinyin().get_initials(device.lab, u'')[0:2] + "-" + str(total).zfill(3))
+        db.session.add(device)
+        flash(u'成功添加设备')
+        return redirect(url_for('index'))
+    return render_template('add_device.html', form=form)
 
 
-#移除设备
+# 移除设备
 @app.route('/remove-device/<int:id>', methods=['GET', 'POST'])
 @login_required
 def remove_device(id):
-	device = Device.query.get_or_404(id)
-	if device.user == User.query.filter_by(username='Admin').first():
-		flash(u'不能删除管理员添加的设备')
-	else:
-		db.session.delete(device)
-		flash(u'成功删除此设备')
-	return redirect(url_for('index'))
+    device = Device.query.get_or_404(id)
+    if device.user == User.query.filter_by(username='Admin').first():
+        flash(u'不能删除管理员添加的设备')
+    else:
+        db.session.delete(device)
+        flash(u'成功删除此设备')
+    return redirect(url_for('index'))
 
-#登录，系统只允许管理员登录
+
+# 登录，系统只允许管理员登录
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	form  = LoginForm()
-	if form.validate_on_submit():
-		user = User.query.filter_by(number=form.number.data).first()
-		if user is not None and user.validate_password(form.password_hash.data): #user.verify_password(form.password.data):
-			if user.role != Role.query.filter_by(name='Admin').first():
-				flash(u'系统只对管理员开放，请联系管理员获得权限！')
-			else:
-				login_user(user, form.remember_me.data)
-				return redirect(url_for('index'))
-		flash(u'用户名或密码错误！')
-	return render_template('login.html', form=form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(number=form.number.data).first()
+        if user is not None and user.validate_password(
+                form.password_hash.data):  # user.verify_password(form.password.data):
+            if user.role != Role.query.filter_by(name='Admin').first():
+                flash(u'系统只对管理员开放，请联系管理员获得权限！')
+            else:
+                login_user(user, form.remember_me.data)
+                return redirect(url_for('index'))
+        flash(u'用户名或密码错误！')
+    return render_template('login.html', form=form)
 
 
 @app.route('/logout')
 @login_required
 def logout():
-	logout_user()
-	flash(u'成功注销！')
-	return redirect(url_for('login'))
+    logout_user()
+    flash(u'成功注销！')
+    return redirect(url_for('login'))
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-	return render_template('404.html'), 404
+    return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
-	return render_template('500.html'), 500
+    return render_template('500.html'), 500
 
-#加载用户的回调函数
+
+# 加载用户的回调函数
 @login_manager.user_loader
 def load_user(user_id):
-	return User.query.get(int(user_id))
+    return User.query.get(int(user_id))
 
 
 '''
 fake
 '''
 
-list = [u'联想启天2100', u'方正文祥600', u'DSP实验箱', u'功率变换器', u'双踪示波器', u'联想电脑845E', u'曙光天阔服务器', u'ZigBee开发套件', u'专业VR镜头', u'投影机']
+list = [u'联想启天2100', u'方正文祥600', u'DSP实验箱', u'功率变换器', u'双踪示波器', u'联想电脑845E', u'曙光天阔服务器', u'ZigBee开发套件', u'专业VR镜头',
+        u'投影机']
+
 
 def fake_user(count=10):
     for i in range(count):
         user = User(username=fake.name(),
-                	number=fake.email(),
-					role_id=2)
+                    number=fake.email(),
+                    role_id=2)
         user.set_password('123456')
         db.session.add(user)
         try:
@@ -262,33 +286,42 @@ def fake_user(count=10):
         except IntegrityError:
             db.session.rollback()
 
+
 def fake_device(count=10):
+    #total = count
     for i in range(count):
         device = Device(name=random.choice(list),
-					user=User.query.get(random.randint(1, User.query.count())),
-					time=fake.date_time_this_year(),
-					lab=fake.company()[:-4]+"实验室")
+                        user=User.query.get(random.randint(1, User.query.count())),
+                        time=fake.date_time_this_year(),
+                        lab=fake.company()[:-4] + "实验室")
+        #device.set_deviceID(
+        #    str(device.time[0:3]) + "-" + str(Pinyin().get_initials(device.lab[0:3], u'')) + "-" + str(i))
+        device.set_deviceID(
+            "2020-" + Pinyin().get_initials(device.lab, u'')[0:2] + "-" + str(i+1).zfill(3))
+        #print("2020-" + Pinyin().get_initials(device.lab, u'')[0:2] + "-" + str(i+1))
         db.session.add(device)
-        try:
-            db.session.commit()
-        except IntegrityError:
-            db.session.rollback()
+        # print(str(device.time[0:3])+"-"+str(Pinyin().get_initials(device.lab[0:3],u''))+"-"+str(i))
+    try:
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
 
 '''
 增加命令'python app.py init' 
 以增加身份与初始管理员帐号
 '''
+
+
 @manager.command
 def init():
-	from app import Role, User
-	db.drop_all()
-	db.create_all()
-	Role.insert_roles()
-	User.generate_admin()
-	fake_user(10)
-	fake_device(15)
+    from app import Role, User
+    db.drop_all()
+    db.create_all()
+    Role.insert_roles()
+    User.generate_admin()
+    fake_user(10)
+    fake_device(15)
 
 
-
-if __name__=='__main__':
-	manager.run()
+if __name__ == '__main__':
+    manager.run()
